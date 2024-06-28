a61bf11a46afcacbbce7772777ef1052662039eb
grenook-prog <frizen.artem.n@gmail.com>
2024-06-28 17:45:34 +0500

    add port variable

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   }));
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(8000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 
   if (module.hot) {
     module.hot.accept();
